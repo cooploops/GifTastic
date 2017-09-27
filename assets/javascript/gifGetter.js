9fb93d610006c190e5c318d9e0fba0808014a3dd
TJ <cooploops@gmail.com>
2017-09-26 22:30:22 -0700

    add styling, condition on not allowing blank searches, and removing old searches when new button is clicked

--- assets/javascript/gifGetter.js
+++ assets/javascript/gifGetter.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     // some initial variables to hold some initial data
     var tvGifs = {
-        topics: ["The Office", "Parks and Rec", "Community", "Silicon Valley", "One Punch Man", "FLCL"],
+        topics: ["The Office", "Parks and Rec", "Community", "Silicon Valley", "One Punch Man", "FLCL", "Portlandia"],
         tvShow: "",
         limit: 10,
         // function to perform AJAX call to giphy API and once done generate image, rating, and column that holds it
@@ -13,14 +13,18 @@ window.onload = function() {
                 })
                 .done(function(response) {
                 	console.log(response);
+                    $(".row").remove();
+                    var tvShowRow = $("<div class='row mx-2 justify-content-center'>");
+                    tvShowRow.attr("id", tvGifs.tvShow)
+                    $("form").append(tvShowRow);
                 	for(i=0;i<tvGifs.limit;i++){
 	                	var rating = response.data[i].rating;
 	                	var mp4 = response.data[i].images.looping.mp4;
 	                	var gif = $("<video class=video>");
 	                	gif.html("<source src='"+mp4+"'></source>")
                         var gifRating = $("<h4>");
-                        gifRating.html("Rating: "+ rating);
-	                	var tvShowColumn =$("<div class='col'>");
+                        gifRating.html("Rating: "+ rating.toUpperCase());
+	                	var tvShowColumn =$("<div class='col align-self-center'>");
                         tvShowColumn.prepend(gifRating);
 	                	tvShowColumn.append(gif);
 	                	$(".row").prepend(tvShowColumn);
@@ -40,7 +44,7 @@ window.onload = function() {
         	$(".tvShowsList").empty();
         	for(i=0;i<tvGifs.topics.length;i++){
         		var newButton = $("<button>");
-        		newButton.addClass("btn btn-info tvShowButton mx-1");
+        		newButton.addClass("btn tvShowButton mx-1 my-1");
         		newButton.attr("type","button");
         		newButton.html(tvGifs.topics[i]);
         		$(".tvShowsList").append(newButton);
@@ -51,26 +55,25 @@ window.onload = function() {
     // generate initals buttons to display
     tvGifs.renderButtons();
 
-    // when you hit enter or click the submit button it'll check to see if it's already there and if it isn't then it will add a button for you
+    // when you hit enter or click the submit button it'll check to see if it's already there THEN if it's blank and if neither then it will add a button for you
     $("#addShow").click(function(event){
     event.preventDefault();
 	var newShow = $("#tvShowName").val().trim();
 	console.log(newShow);
-		if(tvGifs.topics.indexOf(newShow) === -1 ){
-		tvGifs.topics.push(newShow);
-		tvGifs.renderButtons();
+		if(tvGifs.topics.indexOf(newShow) === -1){
+            if(newShow != ""){
+        		tvGifs.topics.push(newShow);
+        		tvGifs.renderButtons();
+            }
 		} else {
-			// do nothing
+			// do nothing for right now
 		}
 	});
 
     // on click function to check for what TV show button you're click and then display the 10 gifs for it
 	$(document).on("click",".tvShowButton",(function(){
-	console.log("here");
 	tvGifs.tvShow = $(this).text();
 	console.log(tvGifs.tvShow);
-	var tvShowRow = $("<div class='row'>");
-	$("form").append(tvShowRow);
 	tvGifs.searchFunction();
 	}));
 
